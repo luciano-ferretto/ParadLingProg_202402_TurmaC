6a2a652f7e9af421a5b324de21a111b8a8300126
luciano.ferretto <luciano.ferretto@atitus.edu.br>
2024-10-18 19:15:58 -0300

    Eureka-service

---
+++ microservices/eureka-service/src/test/java/br/edu/atitus/paradigma/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.paradigma.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
